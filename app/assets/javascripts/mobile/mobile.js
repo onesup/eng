be92fce1975bfa32bd09ef419f49e7d57b9ca004
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-06-20 14:31:19 +0900

    get set poster code

--- app/assets/javascripts/mobile/mobile.js
+++ app/assets/javascripts/mobile/mobile.js
@@ -10,4 +10,28 @@ $(document).ready(function(){
       swt = true;
     }
   });
+  
+  get_poster_code();
+  
 });
+
+
+function get_poster_code(){
+  $(".poster_select").click(function(e){
+    var code = $(this).attr("code");
+    get_poster_btn_href(code);
+  });
+  
+  //console.log(e.result);
+  // return code;
+}
+
+
+function get_poster_btn_href(code){
+    var href = $("#poster_btn").attr("href");   
+    var data_href = $("#poster_btn").attr("data-href");                                
+    href = data_href+"?code="+code;                   
+    $("#poster_btn").attr("href", href);            
+    console.log($("#poster_btn").attr("href"));    
+    
+}
