fd1d5c517756449da123ca4fb773f6b4ea6d36fe
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2014-06-23 23:44:40 +0900

    seed 변경

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 if User.find_by_email("admin@admin.com").nil?
-  User.create!(email:"admin@admin.com", password:"aaaabbbb", name:"관리자", phone:"010-9999-9999", birthday: Time.now) 
+  User.create!(
+    email:"admin@admin.com", password:"aaaabbbb", name:"관리자", phone:"010-9999-9999",
+    address: "서울", address_detail: "광진구") 
 end
