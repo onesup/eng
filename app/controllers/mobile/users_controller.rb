9b6d93fa7974569104f4216b2a255d9f0a14afbd
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-07-12 19:20:23 +0900

    pc address 변경 완

--- app/controllers/mobile/users_controller.rb
+++ app/controllers/mobile/users_controller.rb
@@ -49,8 +49,7 @@ class Mobile::UsersController < ApplicationController
       session[:poster_code] = params[:code]
     end
     stock = User.poster_stock(params[:code])
-  #  if stock <= 0
-    if stock > 0
+    if stock <= 0
       redirect_to mobile_out_of_stock_path
     else
       @user = User.new(poster_code: session[:poster_code])
